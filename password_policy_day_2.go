90d406a70b9c97579c7def76e7d7388634bba62f
woodsjc <woodsjc0@gmail.com>
2020-12-02 11:06:06 -0600

    Adding first and second day.

---
+++ password_policy_day_2.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"regexp"
+)
+
+
+func parseLine(line string) bool {
+	re := regexp.MustCompile(`^(?P<start>\d+)-(?P<end>\d+) (?P<str>\w+): (?P<pass>\w+)$`)
+	m :=	re.FindStringSubmatch(strings.TrimSpace(line))
+	if len(m) < 5 {
+		log.Print("Failed to match: ", m)
+		return false
+	}
+
+	//log.Print("m[0]:", m, " m[1]:", m[1], " m[2]:", m[2], " m[3]:", m[3])
+	sub_re := regexp.MustCompile(m[3])
+	count := len(sub_re.FindAllStringIndex(m[4], -1))
+	start, err := strconv.Atoi(m[1])
+	if err != nil {
+		return false
+	}
+
+	end, err := strconv.Atoi(m[2])
+	if err != nil {
+		return false
+	}
+	
+	if count < start || count > end {
+		return false
+	}
+
+	return true
+}
+
+func parseLinePart2 (line string) bool {
+	re := regexp.MustCompile(`^(?P<start>\d+)-(?P<end>\d+) (?P<str>\w+): (?P<pass>\w+)$`)
+	m :=	re.FindStringSubmatch(strings.TrimSpace(line))
+	if len(m) < 5 {
+		log.Print("Failed to match: ", m)
+		return false
+	}
+
+	start, err := strconv.Atoi(m[1])
+	if err != nil {
+		return false
+	}
+
+	end, err := strconv.Atoi(m[2])
+	if err != nil {
+		return false
+	}
+	
+	is_first := string(m[4])[start-1] != m[3][0]
+	is_second := string(m[4])[end-1] != m[3][0]
+	if (is_first || is_second) && !(is_first && is_second)  {
+		return true 
+	}
+
+	return false
+}
+
+func readInputFile() []string {
+	file_name := "input/day_2_input.txt"
+	file, err := os.Open(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	return strings.Split(string(data), "\n")
+}
+
+func main () {
+	line_array := readInputFile()
+	correct := 0
+	correct_part2 := 0
+
+	for _, line := range line_array {
+		if parseLine(line) {
+			correct++
+		}	
+
+		if parseLinePart2(line) {
+			correct_part2++
+		} else {
+			log.Print("Incorrect line part 2: ", line, "\n")
+		}
+	}
+
+	fmt.Println("Correct lines: ", correct)
+	fmt.Println("Correct lines part 2: ", correct_part2)
+
+	//Correct lines:  548
+	//Correct lines part 2:  502
+}
---
+++ report_repair_day_1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func calcMultiple(num_list []int, target int, ignore int) int64 {
+	val := make(map[int]bool)
+
+	for i:=0; i<len(num_list); i++ {
+		pair := target - num_list[i]
+		
+		if _, in_map := val[pair]; in_map {
+			return int64(num_list[i]) * int64(pair)
+		}
+		
+		if ignore != i {
+			val[num_list[i]] = true
+		}
+	}
+
+	return 0
+}
+
+func calcTripleMultiple(num_list []int, target int) int64 {
+	for i:=0; i<len(num_list); i++ {
+		pair_total := target - num_list[i]
+
+		if tmp := calcMultiple(num_list, pair_total, i); tmp != 0 {
+			return tmp * int64(num_list[i])
+		}
+	}
+
+	return 0
+}
+
+func main () {
+	target := 2020
+	file_name := "input/day_1_input.txt"
+	file, err := os.Open(file_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	to_parse := strings.Split(string(data), "\n")
+
+	num_array := make([]int, len(to_parse))
+	for i, token := range to_parse {
+		num, _ := strconv.Atoi(strings.TrimSpace(token))
+		num_array[i] = num
+	}
+
+	fmt.Println("Two entries summing to ",
+		target, " when multiplied: ", calcMultiple(num_array, target, -1))
+	fmt.Println("Three entries summing to ",
+		target, " when multiplied: ", calcTripleMultiple(num_array, target))
+
+	 //Two entries summing to 2020 when multiplied:  355875
+	 //Three entries summing to 2020 when multiplied:  140379120
+}
+
